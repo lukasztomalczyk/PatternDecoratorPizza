3ed93a3c6f08332d7b48545194d80784d1784ba6
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-05-11 20:10:08 +0200

    work

---
+++ PatternDecoratorPizza/Modules/Decorator/PizzaDecorator.cs
@@ -0,0 +1,21 @@
+﻿namespace PatternDecoratorPizza.Modules.Decorator
+{
+    internal class PizzaDecorator : PizzaBase
+    {
+        private readonly PizzaBase _pizzaBaseImplementation;
+
+        public PizzaDecorator(PizzaBase pizzaBaseImplementation)
+        {
+            this._pizzaBaseImplementation = pizzaBaseImplementation;
+        }
+        public override double CalculatePrice()
+        {
+            return _pizzaBaseImplementation.CalculatePrice();
+        }
+
+        public override string GetTypeOfPizza()
+        {
+            return _pizzaBaseImplementation.GetTypeOfPizza();
+        }
+    }
+}
